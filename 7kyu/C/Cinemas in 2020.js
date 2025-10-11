120b6600479baa7c29bfd2a6c056092fae9f8526
Mark Boots <markboots@outlook.com>
2025-10-11 11:20:45 +0200

    7kyu: update 'Cinemas in 2020.js'

---
+++ 7kyu/C/Cinemas in 2020.js
@@ -0,0 +1,26 @@
+/*
+Cinemas in 2020
+https://www.codewars.com/kata/68b0064510c5854a66e6323a/train/javascript
+
+Given an array of seats, return the maximum number of new people which can be seated, as long as there is at least a gap of 2 seats between people.
+
+Empty seats are given as 0.
+Occupied seats are given as 1.
+Don't move any seats which are already occupied, even if they are less than 2 seats apart. Consider only the gaps between new seats and existing seats.
+Examples
+[0, 0, 0, 1, 0, 0, 1, 0, 0, 0] ➞ 2
+// [1, 0, 0, 1, 0, 0, 1, 0, 0, 1]
+
+[0, 0, 0, 0] ➞ 2
+// [1, 0, 0, 1]
+
+[1, 0, 0, 0, 0, 1] ➞ 0
+// There is no way to have a gap of at least 2 chairs when adding someone else.
+Notes
+Notice how there may be several possibilities for assigning seats to people, but these cases won't affect the results.
+All seats will be valid.
+
+*/
+maximumSeating=s=>s.reduce((c,_,i)=>!s[i]&&!s[i-1]&&!s[i+1]&&!s[i-2]&&!s[i+2]?(s[i]=1,c+1):c,0)
+// Alternative solution using regex
+maximumSeating=s=>(s.join``.match(/(?:^|00)0(?=00|$)|^0{1,3}$/g)||[]).length
